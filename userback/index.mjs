364a2ff4627d52c619d2388dd0e2d21c07ba49b6
Bardia Jedi <bardiajedi@gmail.com>
2022-09-13 11:33:51 +0200

    added dotenv config

--- userback/index.mjs
+++ userback/index.mjs
@@ -1,8 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
-import { Users } from "./User.mjs"
 import { router } from "./routes/users.js"
+import { config } from "dotenv";
 
+config()
+
+if (!process.env.MONGO_URL)
+    throw 'MONGO_URL is required!!'
 
 const app = express();
 const port = 3000;
@@ -17,13 +21,13 @@ app.use('/users', router)
 
 
 const mongoStart = async () => {
-    console.log("Connecting...", process.env.MONGO_URL); //'mongodb://localhost:27017/test'  $env:MONGO_URL='mongodb://localhost:27017/test'
+    console.log("Connecting...", process.env.MONGO_URL);
     await mongoose.connect(process.env.MONGO_URL)
     app.listen(port, () => console.log("listening on port " + port))
     console.log("Connected!", process.env.MONGO_URL);
 }
 
-mongoStart().then(_ => console.log("Finished"))
+mongoStart().then(() => console.log("Finished"))
 console.log("mongo");
 
 export default app

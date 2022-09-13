49a294d8186bb6d58f8f910c4a063fed1a4f96ce
Bardia Jedi <bardiajedi@gmail.com>
2022-09-13 11:32:41 +0200

    refactor

--- userback/apiTest/users.test.js
+++ userback/apiTest/users.test.js
@@ -2,9 +2,7 @@ import request from "supertest";
 import app from "../index.mjs";
 import crypto from "crypto";
 
-const users = [
-
-];
+const users = [];
 
 app.get("/users", (req, res) => {
   return res.status(200).json({
@@ -22,17 +20,17 @@ describe("GET /users", () => {
     email: "test.testsson@hotmail.com",
     phoneNumber: "0732020202",
     adress: "mojodoogatan 3",
-    role: "User"
-  }
+    role: "User",
+  };
   beforeAll(async () => {
-    await request(app).post("/users").send(newUser)
-    console.log(newUser)
-    console.log(process.env)
-  })
+    await request(app).post("/users").send(newUser);
+    console.log(newUser);
+    console.log(process.env.MONGO_URL);
+  });
   afterAll(async () => {
-    await request(app).delete(`/users/${newUser._id}`)
-    console.log(newUser._id)
-  })
+    await request(app).delete(`/users/${newUser._id}`);
+    console.log(newUser._id);
+  });
   it("should return 200", async () => {
     const response = await request(app).get("/users");
     expect(response.statusCode).toBe(200);
@@ -40,8 +38,8 @@ describe("GET /users", () => {
   it("should return users", async () => {
     const response = await request(app).get("/users");
     expect(response.body.length >= 1).toBe(true);
-  })
-})
+  });
+});
 
 app.post("/users", (req, res) => {
   try {
